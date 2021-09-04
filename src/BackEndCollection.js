a3d8707f9e39567cbc5f887ec18cd74132ea61e1
yanJuicy <hanjub3@gmail.com>
2021-09-04 21:35:39 +0900

    [#33] develop: BackEnd Database 내용 수정

--- src/BackEndCollection.js
+++ src/BackEndCollection.js
@@ -3,7 +3,7 @@ import { internetCollection } from './data/internet.js';
 import { basicFrontendCollection } from './data/basic-frontend-knowledge.js';
 import { OSCollection } from './data/os-knowledge.js';
 import { backendLanguageCollection } from './data/backend-languages.js';
-import { databasesCollection } from './data/databases.js';
+import { backendDatabasesCollection } from './data/backend-databases.js';
 
 /**
  * All available collections.
@@ -14,5 +14,5 @@ export const collections = [
   basicFrontendCollection,
   OSCollection,
   backendLanguageCollection,
-  databasesCollection,
+  backendDatabasesCollection,
 ];
